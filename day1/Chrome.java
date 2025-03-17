fe7b8be877d688e4e29c556fdcfadb94e1b76ea1
geethasrithirupathi <76461552+geethasrithirupathi@users.noreply.github.com>
2025-03-17 19:30:15 +0530

    Add files via upload
    day1 task completed

---
+++ day1/Chrome.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		
+		float versionOfType=91.0f;
+		String developerOfType="Google";
+		Boolean isBetaOfType = true;
+		int releaseYearOfType = 2008;
+		char shortcutOfType ='c';
+		System.out.println(versionOfType+ " "+developerOfType+" "+isBetaOfType+" "+releaseYearOfType+" "+shortcutOfType);
+	}		
+
+	}
+
+
